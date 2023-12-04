2bda2e4d700736b8deff9497d543af5f750a71dd
twoballs <twoballs666@gmail.com>
2023-12-05 02:27:23 +0800

    init

---
+++ include/toy/ToyDialect.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "mlir/IR/BuiltinDialect.h"
+#include "toy/ToyDialect.h.inc" // include 进来就可以了
+#include "ToyOps.h"
+#define GET_OP_CLASSES
---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include "mlir/IR/AsmState.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/Parser/Parser.h"
+#include "mlir/Support/FileUtilities.h"
+#include "toy/ToyDialect.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace mlir;
+
+int main(int argc, char ** argv) {
+    MLIRContext ctx;
+    ctx.loadDialect<func::FuncDialect, 
+                    arith::ArithDialect, 
+                    toy::ToyDialect>();
+
+    // 创建 OpBuilder
+    OpBuilder builder(&ctx);
+    auto mod = builder.create<ModuleOp>(builder.getUnknownLoc());
+
+    // 设置插入点
+    builder.setInsertionPointToEnd(mod.getBody());
+
+    // 创建 func
+    auto i32 = builder.getI32Type();
+    auto funcType = builder.getFunctionType({i32, i32}, {i32});
+    auto func = builder.create<func::FuncOp>(builder.getUnknownLoc(), "test", funcType);
+
+    // 添加基本块
+    auto entry = func.addEntryBlock();
+    auto args = entry->getArguments();
+
+    // 设置插入点
+    builder.setInsertionPointToEnd(entry);
+
+    // 创建 arith.addi
+    auto addi = builder.create<arith::AddIOp>(builder.getUnknownLoc(), ValueRange(args));
+
+    // 创建 func.return
+    builder.create<func::ReturnOp>(builder.getUnknownLoc(), ValueRange(addi));
+
+    builder.setInsertionPoint(addi);
+    builder.create<arith::MulIOp>(builder.getUnknownLoc(), ValueRange(args))->erase();
+    builder.create<toy::AddOp>(builder.getUnknownLoc(), i32, args[0], args[1]);
+    mod->print(llvm::outs());
+    return 0;
+}
