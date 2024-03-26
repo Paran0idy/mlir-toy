35c8acf87c822c86243d09ac6c100a193307c427
Jiaxing Ding <61589029+Paran0idy@users.noreply.github.com>
2024-03-27 02:08:32 +0800

    Delete build directory

--- build/lib/Transforms/CMakeFiles/ToyTransforms.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libToyTransforms.a"
-  "libToyTransforms.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/ToyTransforms.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
